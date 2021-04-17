5e6387c5ecc7960518f0af64edc3da6a122230d5
barbarbar338 <demirci.baris38@gmail.com>
2021-04-17 22:08:10 +0300

    🚑️ Update package name

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
-package main
+package snowflake
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"time"
 )
@@ -20,28 +19,28 @@ type DeconstructedSnowflake struct {
 	binary string
 }
 
-func (snowflake *Snowflake) Generate() (string, error) {
+func (s *Snowflake) Generate() (string, error) {
 	now := time.Now().UnixNano() / 1000000;
-	if snowflake.INCREMENT >= 4095 {
-		snowflake.INCREMENT = 0;
+	if s.INCREMENT >= 4095 {
+		s.INCREMENT = 0;
 	}
 	difference_bin := StringPadStart(strconv.FormatInt(
-			(now - snowflake.EPOCH),
+			(now - s.EPOCH),
 			2,
 		),
 		42,
 		"0",
 	) 
 	increment_bin := StringPadStart(strconv.FormatInt(
-		snowflake.INCREMENT,
+		s.INCREMENT,
 		2,
 	), 12, "0");
-	snowflake.INCREMENT++;
+	s.INCREMENT++;
 	bin := difference_bin + "0000100000" + increment_bin;
 	return BinaryToID(bin);
 }
 
-func (snowflake *Snowflake) Deconstruct(id string) (DeconstructedSnowflake, error) {
+func (s *Snowflake) Deconstruct(id string) (DeconstructedSnowflake, error) {
 	binary, err := IDToBinary(id);
 	if err != nil {
 		return DeconstructedSnowflake {}, errors.New("unable to parse binary from ID");
@@ -72,32 +71,10 @@ func (snowflake *Snowflake) Deconstruct(id string) (DeconstructedSnowflake, erro
 	return deconstructed, nil;
 }
 
-func (snowflake *Snowflake) IsSnowflake(id string) bool {
-	deconstructed, err := snowflake.Deconstruct(id);
+func (s *Snowflake) IsSnowflake(id string) bool {
+	deconstructed, err := s.Deconstruct(id);
 	if err != nil {
 		return false;
 	}
-	return deconstructed.timestamp > snowflake.EPOCH && deconstructed.timestamp <= 3619093655551;
+	return deconstructed.timestamp > s.EPOCH && deconstructed.timestamp <= 3619093655551;
 }
-
-func main() {
-	snowflake := Snowflake {
-		EPOCH: 1618606800,
-	}
-	fmt.Println(snowflake.Deconstruct("6782465263234318336"));
-}
-/*
-	ID: 6782465263234318336
-
-	Binary: 101111000100000001010000011100011001100100000100000000000000000
-	Timestamp: 1617065730866 
-	WorkerID: 1 
-	ProcessID: 0 
-	Increment: 0
-
-	●----------------------------------------------------------------------●
-
-	  ╔ 								Binary								   ╗
-	 ║10111100010000000101000001110001100110010║ ║00001║ ║00000║ ║000000000000║
-	╚				Timestamp				  ╝	╚ WID ╝	╚ PID ╝	╚  Increment ╝
-*/
